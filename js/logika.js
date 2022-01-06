80430529e010122a32f1a1e3d3c78da23e5e7b45
Alfian Reno Prasetyo <alfianreno2@gmail.com>
2022-01-06 17:41:17 +0700

    Add files via upload

---
+++ js/logika.js
@@ -0,0 +1,37 @@
+let a = true;
+let b = false;
+let c = true;
+let d = false;
+
+// OR akan menjadi true jika salah satu pembanding bernilai true
+console.log(a || b); 
+// AND akan bernilai false jika salah satu pembanding bernilai false
+console.log(a && b);
+
+let x = 8;
+let y = 7;
+
+// Lebih besar
+console.log(x > y);
+// Pembanding diatas akan bernilai "true", karena variabel 'x' memang bernilai lebih besar daripada variabel 'y'
+
+// Lebih kecil
+console.log(x < y);
+// Pembanding diatas akan bernilai "false", karena variabel 'x' tidak bernilai lebih kecil daripada variabel 'y'
+
+// Sama dengan
+console.log(x == y);
+// Pembanding diatas akan bernilai "false", karena variabel 'x' memang bernilai sama daripada variabel 'y'
+
+// Lebih besar sama dengan
+console.log(x >= y);
+// Pembanding diatas akan bernilai "true", karena variabel 'x' memang bernilai lebih besar sama dengan daripada variabel 'y'
+
+// Lebih kecil sama dengan
+console.log(x <= y);
+// Pembanding diatas akan bernilai "false", karena variabel 'x' tidak bernilai lebih besar daripada variabel 'y'
+
+// Tidak sama dengan
+console.log(x != y);
+// Pembanding diatas akan bernilai "true", karena variabel 'x' tidak bernilai sama dengan variabel 'y'
+
---
+++ js/tipedata.js
@@ -0,0 +1,24 @@
+let angka = 10;
+
+var kata = "saya belajar javascript";
+var nama = "reno";
+
+let desimal = 4.2;
+
+let kondisi = true;
+
+var umur = null;
+
+let kosong;
+
+console.log(typeof angka);
+console.log(angka);
+console.log(typeof kata);
+console.log(kata);
+console.log(typeof desimal);
+console.log(desimal);
+console.log(1/0);
+console.log("Nama saya" +nama);
+console.log(kondisi);
+console.log(umur);
+console.log(kosong);
